7de42b1a4185db7d03279c2a936be1bae155490f
developper <backend@temp.com>
2023-08-06 07:11:00 +0000

    DB周り完了

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 app_name="blogapp"
 urlpatterns = [
-    path("" , views.IndexView.as_view() , name="index")
+    path("" , views.IndexView.as_view() , name="index"),
+    
+    path(
+        "blog-detail/<int:pk>/" ,
+        views.BlogDetail.as_view(),
+        name="blog_detail"
+    )
 ]
